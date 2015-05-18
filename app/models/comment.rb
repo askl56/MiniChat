e991f162291ae95bdfcd1c264531e3b5d0be70f2
askl56 <amscott@openmailbox.org>
2015-05-18 04:00:07 +0100

    Notificatons

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   validates :body, presence: true, length: {maximum: 2000}
+  after_create :notify_comment_added
 
   def timestamp
     created_at.strftime('%-d %B %Y, %H:%M:%S')
-
   end
 
   class << self
@@ -13,5 +13,20 @@ class Comment < ActiveRecord::Base
         order('created_at ASC').limit(all.count - 50).destroy_all
       end
     end
+
+    def on_change
+      Comment.connection.raw_connection.wait_for_notify do |event, pid, comment|
+        yield comment
+      end
+    end
+
+  ensure
+    Comment.connection.execute "UNLISTEN comments"
+  end
+
+  private
+
+  def notify_comment_added
+    Comment.connection.execute "NOTIFY comments, 'data'"
   end
 end
